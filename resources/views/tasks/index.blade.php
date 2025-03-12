ccef226ea1fdfe45bb25b42e8079bf8832853a3a
Icaro <icaromm12@gmail.com>
2025-03-12 14:09:31 -0300

    o inicio de um sonho

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
+<body class="flex">
+    <div class="w-xs h-screen bg-[#131e33]">
+
+    </div>
+    <div class="w-full h-screen bg-[#0F172A]"></div>
+
+</body>
+</html>
