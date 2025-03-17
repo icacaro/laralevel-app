0b7440c279317bd9f9c49e44ac7bb0ef28ff7bcd
Icaro <icaromm12@gmail.com>
2025-03-17 13:22:08 -0300

    +

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,9 +7,17 @@
 </head>
 <body class="flex">
     <div class="w-xs h-screen bg-[#131e33]">
-
+        <div class="w-full h-16 bg-red-500 flex justify-center items-center text-xl uppercase font-bold">
+            <h2>to do <span class="text-gray-500">dev</span>code</h2>
+        </div>
     </div>
-    <div class="w-full h-screen bg-[#0F172A]"></div>
+    <div class="w-full h-screen bg-[#0F172A]">
+        <div class="w-full h-16 bg-blue-500 flex items-center justify-between px-6">
+            <input type="text" class="w-xs bg-[#141E33] py-2 text-[#94A3BB] pl-2 rounded-md hover:border-[#6D28D9]" placeholder="Pesquisar">
+            <p>Execução</p>
+            <button> Adicionar Tarefa</button>
+        </div>
+    </div>
 
 </body>
 </html>
